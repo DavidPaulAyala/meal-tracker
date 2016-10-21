d76a070596e570ad75f54e30d8b654a22f4a4cdc
David Ayala <dpayala@hotmail.com>
2016-10-21 07:47:07 -0700

    add intial setup files

---
+++ app/EXAMPLE.model.ts
@@ -0,0 +1,4 @@
+// export class ExampleObject {
+//     public exampleProperty1: boolean = false;
+//     constructor(public exampleProperty2: string, public exampleProperty3: number = 11) { }
+// }
